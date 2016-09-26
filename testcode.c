cadb3f9d7bdc585eeef7d2ff5671416a968da721
tagumi <cmcjnz@gmail.com>
2016-09-26 04:27:18 -0400

    here we go again

--- testcode.c
+++ testcode.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 
 main(){
-	printf("aaaaaaaaaa");
+	int a;
+	printf("How many times shall we print Cameron's name?: ");
+	scanf("%d", &a);
+	for (int i = 1; i <= a; i++){
+		printf("Cameron\n");
+		sleep(1);
+	}
+	
+return 0;
+		
 }
